7dd70ae51d1f097839a0791ba759b325ae30ad96
Alief12221061 <12221061@bsi.ac.id>
2023-10-19 14:08:47 +0700

    memperbaiki link pada header

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -1,25 +1,29 @@
 <?php
 class Matakuliah extends CI_Controller
+
 {
  public function index()
  {
  $this->load->view('view-form-matakuliah');
+
  }
+ 
  public function cetak()
  {
  $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
-'required|min_length[3]', [
+'required|min_length[3]', 
+[
  'required' => 'Kode Matakuliah Harus diisi',
- 'min_lenght' => 'Kode terlalu pendek'
+ 'min_length' => 'Kode terlalu pendek'
  ]);
- $this->form_validation->set_rules('nama', 'Nama Matakuliah', 
-'required|min_length[3]', [
- 'required' => 'Nama Matakuliah Harus diisi',
- 'min_lenght' => 'Nama terlalu pendek'
- ]);
- if ($this->form_validation->run() != true) {
- $this->load->view('view-form-matakuliah');
- } else {
+ $this->form_validation->set_rules('nama', 'Nama Matakuliah',
+ 'required|min_length[3]', 
+ [
+'required' => 'Nama Matakuliah Harus diisi',
+'min_length' => 'Nama terlalu pendek'
+]);
+ if ($this->form_validation->run() != true) {$this->load->view('view-form-matakuliah');} 
+ else {
  $data = [
  'kode' => $this->input->post('kode'),
  'nama' => $this->input->post('nama'),
@@ -27,6 +31,7 @@ class Matakuliah extends CI_Controller
  ];
  $this->load->view('view-data-matakuliah', $data);
  }
+
  }
 }
 ?>
